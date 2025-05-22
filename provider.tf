1f0fb9af88283be88705d2e46d3f6a8023f9de29
Jasmijn Emilia Rosalina Knoope (@x86-39) <x86-39@39.systems>
2025-05-22 11:36:43 +0200

    Version bump

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     hcloud = {
       source = "hetznercloud/hcloud"
-      version = "~> 1.44.0"
+      version = "~> 1.51.0"
     }
     ansible = {
-      version = "~> 1.0.0"
+      version = "~> 1.3.0"
       source  = "ansible/ansible"
     }
   }
